0cefd35227a9cc80d686e58490441e2123b5f0e4
F13Fabricio <fabricio.13.new@gmail.com>
2017-10-13 16:37:18 -0300

    Adicionada a funcionalidade de mapear um perfil

--- src/br/com/f13fabricio/uffmail/ProfileManager.java
+++ src/br/com/f13fabricio/uffmail/ProfileManager.java
@@ -1,5 +1,10 @@
 package br.com.f13fabricio.uffmail;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /* 
  * O aluno pode está com o status Ativo ou Inativo.
  * Somente o status como Ativo permite ao estudante solicite a criação de um UFFMail.
@@ -10,6 +15,7 @@ public class ProfileManager {
 	private static ProfileManager profileManager;
 	
 	private String dataBase;
+	private String csvRegex = ",";
 	
 	private ProfileManager() { }
 	
@@ -22,6 +28,25 @@ public class ProfileManager {
 	
 	public void setDataBase(String csvFile) {
 		dataBase = csvFile;
+	}
+	
+	public void setCsvRegex(String regex) {
+		this.csvRegex = regex;
+	}
+	
+	public StudentProfile mapProfile(String enrollment) throws NullPointerException, IOException {
+		String line = "";
+		/*Se a base de dados não informada é lançada uma NullPoiterException*/
+		if (dataBase == null) 
+			throw new NullPointerException("Profile Manager não vinculado com uma base de dados.");
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(dataBase))) {
+			
+			while ((line = br.readLine()) != null) {
+				String[] fields = line.split(csvRegex);
+				return new StudentProfile(fields);
+			}
+		}
 	}
 
 }
